29790190a84e20361b38b33595862a5989d34e4c
shami <shamiabdul228@gmail.com>
2024-09-20 00:02:08 +0530

    commit

---
+++ bin/sample.dart
@@ -0,0 +1,10 @@
+void main(){
+  print("welcome");
+  print(12);
+  print(6.9);
+  print(true);
+  print("shameema");
+  print("23");
+  print("manoor");
+  print("flutter");
+}
---
+++ bin/variables.dart
@@ -0,0 +1,17 @@
+void main(){
+  String name="shami";
+  print("My name is $name");
+  name="manu";
+  print("My name is $name");
+  int age=25;
+  print("My age is $age");
+  double mark=25.5;
+  print(mark);
+  int a=20;
+  int b=5;
+  int sum=a+b;
+  print("sum $sum");
+  int c=10;
+  int d=20;
+  print("sum= $c+$d");
+}
